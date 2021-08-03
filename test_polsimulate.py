8de3f37f27289e197d77d64d929c8ed29cf4429c
Michael Olberg <michael.olberg@chalmers.se>
2021-08-03 15:55:02 +0200

    add test procedure

---
+++ test_polsimulate.py
@@ -0,0 +1,18 @@
+model_image = ['SFits.im', 'QFits.im', 'UFits.im', 'Zero.im']
+I = [0.2]
+Q = [0.03]
+U = [0.01]
+V = [0.0]
+RM = [1.e7]
+spec_index = [0.0]
+spectrum_file = ''
+LO = 100.e9
+BBs = [0.]
+spw_width = 2.e9
+nchan = 16
+onsource_time = 0.5
+observe_time = 3.0
+visib_time = '6s'
+nscan = 1
+corrupt = False
+polsimulate()
